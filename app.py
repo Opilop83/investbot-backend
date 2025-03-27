679bdd4d5d65514c401c1eafb6c36b1d61149e48
Opilop83 <opilowskipiotr@gmail.com>
2025-03-27 13:41:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,8 @@ def log():
             return f.read()
     except Exception as e:
         return str(e), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
