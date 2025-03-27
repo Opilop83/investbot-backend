cf924dc66082b7f14c7c5babec1f311afa7cde65
Opilop83 <opilowskipiotr@gmail.com>
2025-03-27 13:46:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import threading
 import subprocess
 import os
 
 app = Flask(__name__)
+CORS(app)
 bot_process = None
 
 @app.route("/status")
